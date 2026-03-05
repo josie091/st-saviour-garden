70495397313b822d7f3ca4606c87d40aad99d6b6
jjurado <jjurado28@stsaviour.org>
2026-03-05 11:24:54 -0500

    lab 5

--- garden.py
+++ garden.py
@@ -3,4 +3,30 @@
 if __name__ == '__main__':
 
     # print without a line break using this function
-    print('🌷', end='')
+    # print('🌷', end='')
+
+    print('solid')
+    for i in range(11):
+        for j in range(11): 
+            print('🌷', end= '')
+        print('')
+
+    print('horizontal')
+    for i in range(11):
+        for j in range(11):
+            if i % 2 == 0:
+                print('🌷', end='')
+            else:
+                print('🌱', end='')
+        print('')
+
+    print('vertical')
+    for i in range (11):
+        for j in range (11):
+            if j % 2 ==0:
+                print ('🌷', end='')
+            else:
+                print('🌱', end='')
+        print ('')
+
+        
